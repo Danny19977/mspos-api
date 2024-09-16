6079177fdf540dc755938658cf7d0cee4fb63908
Germain kataku <katakugermain@gmail.com>
2024-09-16 15:02:40 +0100

    edit table view

--- controllers/dashboard/nd-dashboard.controller.go
+++ controllers/dashboard/nd-dashboard.controller.go
@@ -13,20 +13,20 @@ func NdTableView(c *fiber.Ctx) error {
 
 	sql1 := `
 	SELECT areas.name AS area, 
-		(SUM(eq1) / COUNT("pos_forms"."id")) * 100 AS eq,
-		(SUM(dhl1) / COUNT("pos_forms"."id")) * 100 AS dhl, 
-		(SUM(ar1) / COUNT("pos_forms"."id")) * 100 AS ar, 
-		(SUM(sbl1)/ COUNT("pos_forms"."id")) * 100 AS sbl, 
-		(SUM(pmf1) / COUNT("pos_forms"."id")) * 100 AS pmf,
-		(SUM(pmm1) / COUNT("pos_forms"."id")) * 100 AS pmm, 
-		(SUM(ticket1) / COUNT("pos_forms"."id")) * 100 AS ticket, 
-		(SUM(mtc1) / COUNT("pos_forms"."id")) * 100 AS mtc, 
-		(SUM(ws1) / COUNT("pos_forms"."id")) * 100 AS ws, 
-		(SUM(mast1) / COUNT("pos_forms"."id")) * 100 AS mast,
-		(SUM(oris1) / COUNT("pos_forms"."id")) * 100 AS oris, 
-		(SUM(elite1) / COUNT("pos_forms"."id")) * 100 AS elite,
-		(SUM(yes1) / COUNT("pos_forms"."id")) * 100 AS yes, 
-		(SUM(time1) / COUNT("pos_forms"."id")) * 100 AS time
+		ROUND(SUM(eq1) / COUNT("pos_forms"."id") * 100) AS eq,
+		ROUND(SUM(dhl1) / COUNT("pos_forms"."id") * 100) AS dhl, 
+		ROUND(SUM(ar1) / COUNT("pos_forms"."id") * 100) AS ar, 
+		ROUND(SUM(sbl1)/ COUNT("pos_forms"."id") * 100) AS sbl, 
+		ROUND(SUM(pmf1) / COUNT("pos_forms"."id") * 100) AS pmf,
+		ROUND(SUM(pmm1) / COUNT("pos_forms"."id") * 100) AS pmm, 
+		ROUND(SUM(ticket1) / COUNT("pos_forms"."id") * 100) AS ticket, 
+		ROUND(SUM(mtc1) / COUNT("pos_forms"."id") * 100) AS mtc, 
+		ROUND(SUM(ws1) / COUNT("pos_forms"."id") * 100) AS ws, 
+		ROUND(SUM(mast1) / COUNT("pos_forms"."id") * 100) AS mast,
+		ROUND(SUM(oris1) / COUNT("pos_forms"."id") * 100) AS oris, 
+		ROUND(SUM(elite1) / COUNT("pos_forms"."id") * 100) AS elite,
+		ROUND(SUM(yes1) / COUNT("pos_forms"."id") * 100) AS yes, 
+		ROUND(SUM(time1) / COUNT("pos_forms"."id") * 100) AS time
 	FROM pos_forms
 	INNER JOIN areas ON pos_forms.area_id=areas.id
 	INNER JOIN provinces ON pos_forms.province_id=provinces.id
@@ -49,7 +49,7 @@ func NdByYear(c *fiber.Ctx) error {
 
 	sql1 := `  
 	SELECT EXTRACT(MONTH FROM "pos_forms"."created_at") AS month,
-		CAST(SUM(eq1) / COUNT(*) as decimal(40,0)) * 100 AS eq
+		CAST(SUM(eq1) / COUNT(*) as decimal(40,0) * 100) AS eq
 	FROM pos_forms
 	INNER JOIN provinces ON pos_forms.province_id=provinces.id
 		WHERE "provinces"."name"=? AND EXTRACT(YEAR FROM "pos_forms"."created_at") = EXTRACT(YEAR FROM CURRENT_DATE)
