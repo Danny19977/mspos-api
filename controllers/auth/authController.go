659cd3d2dbdc7359e1b0b1ed0defa10f98389a5d
Germain kataku <katakugermain@gmail.com>
2024-12-17 17:08:54 +0100

    delete SameSite

--- controllers/auth/authController.go
+++ controllers/auth/authController.go
@@ -118,13 +118,13 @@ func Login(c *fiber.Ctx) error {
 	cookie := fiber.Cookie{
 		Name:   "token",
 		Value:  token,
-		Path:   "/",
-		Domain: "*.onrender.com",
+		// Path:   "/",
+		Domain: ".onrender.com",
 		// Domain: "localhost",
 		Expires:  time.Now().Add(time.Hour * 24), //1 day ,
 		HTTPOnly: true,
 		Secure:   true,
-		SameSite: "none",
+		// SameSite: "none",
 		// SessionOnly: true,
 	}
 
