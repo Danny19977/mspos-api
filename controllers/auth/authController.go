0fa9901be7f105a664adfc18a6f2b1c41b1a9484
Germain kataku <katakugermain@gmail.com>
2024-12-17 18:23:02 +0100

    ddd

--- controllers/auth/authController.go
+++ controllers/auth/authController.go
@@ -119,7 +119,7 @@ func Login(c *fiber.Ctx) error {
 		Name:   "token",
 		Value:  token,
 		Path:   "/",
-		Domain: "mspos-api-dvp8.onrender.com",
+		// Domain: "mspos-api-dvp8.onrender.com",
 		// Domain: "localhost",
 		Expires:  time.Now().Add(time.Hour * 24), //1 day ,
 		HTTPOnly: true,
